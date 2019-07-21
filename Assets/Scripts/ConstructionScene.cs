ef48593191b4507858bd104496ca4ea8d107b0e9
kurtome <kurt@melby.me>
2019-07-21 16:28:53 -0400

    adding basic assets and car animation

--- Assets/Scripts/ConstructionScene.cs
+++ Assets/Scripts/ConstructionScene.cs
@@ -7,27 +7,47 @@ using Lean.Touch;
 public class ConstructionScene : MonoBehaviour
 {
 
+  public GameObject Car;
+  public Vector3 carStartScale = new Vector3(0.3f, 0.3f, 1f);
+  public Vector3 carEndScale = new Vector3(1f, 1f, 1f);
+  public float carScaleDuration = 1f;
   public GameObject StopSign;
   private BoxCollider2D signBoxCollider;
+  private SpriteRenderer signSpriteRender;
+  public Sprite signFront;
+  public Sprite signBack;
+  private Boolean isSignBack = false;
+  private DateTime signDoneChangingAfter = DateTime.Now;
+  private DateTime sceneStartTime;
 
   // Start is called before the first frame update
   void Start()
   {
+    sceneStartTime = DateTime.Now;
+
     LeanTouch.OnFingerSwipe += Swiped;
 
     Debug.Log(StopSign.GetInstanceID().ToString());
     signBoxCollider = StopSign.GetComponent<BoxCollider2D>();
+    signSpriteRender = StopSign.GetComponent<SpriteRenderer>();
     Debug.Log(signBoxCollider.GetInstanceID().ToString());
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    float delta = Mathf.Clamp((DateTime.Now - sceneStartTime).Milliseconds, 0f, carScaleDuration * 1000) / (carScaleDuration * 1000);
+    Debug.Log(delta);
+    Car.transform.localScale = Vector3.Lerp(carStartScale, carEndScale, delta);
   }
 
   void Swiped(LeanFinger finger)
   {
+    if (DateTime.Now < signDoneChangingAfter)
+    {
+      Debug.Log("still changing");
+      return;
+    }
     Debug.Log("--------------------------");
     Debug.Log("SWIPED");
 
@@ -37,11 +57,23 @@ public class ConstructionScene : MonoBehaviour
     var distance = heading.magnitude;
     var direction = heading / distance;
     RaycastHit2D[] hits = Physics2D.RaycastAll(swipeStart, direction);
+
     foreach (var hit in hits)
     {
       if (hit.collider == signBoxCollider)
       {
-        StopSign.SetActive(!StopSign.activeSelf);
+        signDoneChangingAfter = DateTime.Now.AddSeconds(1);
+
+        isSignBack = !isSignBack;
+        Debug.Log(isSignBack);
+        if (isSignBack)
+        {
+          signSpriteRender.sprite = signBack;
+        }
+        else
+        {
+          signSpriteRender.sprite = signFront;
+        }
         Debug.Log("hit sign");
       }
       else
